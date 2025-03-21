73f5c6aaaca53e833b722aadf390c4f9fe4e3ec5
andresgrigo <andresgrigorenko@hotmail.com>
2025-03-21 13:02:49 +0100

    Update base URL for Product API to include versioning

--- product-service/src/main/java/com/bytercoder/ecommerce/product/controller/ProductController.java
+++ product-service/src/main/java/com/bytercoder/ecommerce/product/controller/ProductController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 @Tag(name = "Product API", description = "Operations related to products")
 public class ProductController {
